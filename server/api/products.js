88decfdbf82ebdde2e900e33a4e1b0ebc83d1bdf
Lindsay Sack <lindsay.sack@gmail.com>
2018-06-06 16:02:27 -0400

    done with ticket 9

--- server/api/products.js
+++ server/api/products.js
@@ -1,6 +1,22 @@
 const router = require('express').Router()
 const {Product} = require('../db/models')
 
+//route to find single product by ID
+router.param('productId', (req, res, next, id) => {
+  Product.findById(id)
+  .then(product => {
+    if (!product) {
+      const err = Error('Product not found');
+      err.status = 404;
+      throw err
+    }
+    req.product = product;
+    next();
+    return null;
+  })
+  .catch(next);
+});
+
 //All Products Route
 router.get('/', (req, res, next) => {
   Product.findAll({})
@@ -11,11 +27,25 @@ router.get('/', (req, res, next) => {
 //Product by Name route
 router.get('/:name', (req, res, next) => {
   const name = req.params.name;
-
   Product.findByName(name)
   .then((products) => {
     res.json(products)
   })
+  .catch(next)
+})
+
+//Creat New Product Route
+router.post('/', (req, res, next) => {
+  Product.create(req.body)
+  .then(product => res.status(201).json(product))
+  .catch(next)
+})
+
+//Update Product Route
+router.put('/:productId', (req, res, next) => {
+  req.product
+  .update(req.body)
+  .then(product => res.json(product))
   .catch(next)
 })
 
