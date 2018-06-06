15552c2b911ce7e555b8fbac861e3509b13cad52
Chris Perez <chrisaugustusp@gmail.com>
2018-06-06 17:53:01 -0400

    All-Products ComponentComplete

--- client/components/products/all-products.js
+++ client/components/products/all-products.js
@@ -9,30 +9,38 @@ import PropTypes from 'prop-types'
 const Products = (props) => {
   const products = props.state.products;
 
-  return (
-    <div>
-      <h3>Products</h3>
+  if (products.length === 0) {
+    return (
       <div>
-        <h3>There are no products to display</h3>
+        <h3>Products</h3>
+        <div>
+          <h3>There are no products to display</h3>
+        </div>
       </div>
-      {/* <SingleProduct /> */}
-      <div className="parent-product-container">
-        {products.map((product) => {
-          let productImage = {
-            backgroundImage: `url('images/${product.imgUrl}')`
-          }
-          return (
-            <div key={product.id} className="product-container">
-              <div className="product-name">{product.name}</div>
-              <div className="product-imgUrl" style={productImage} />
-              <div className="product-description">{product.description}</div>
-              <div className="product-rating">{product.rating}</div>
-            </div>
-          )
-        })}
+    )
+  }
+  else {
+    return (
+      <div>
+        <h3>Products</h3>
+        <div className="parent-product-container">
+          {products.map((product) => {
+            let productImage = {
+              backgroundImage: `url('images/${product.imgUrl}')`
+            }
+            return (
+              <div key={product.id} className="product-container">
+                <div className="product-name">{product.name}</div>
+                <div className="product-imgUrl" style={productImage} />
+                <div className="product-description">{product.description}</div>
+                <div className="product-rating">{product.rating}</div>
+              </div>
+            )
+          })}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 //THESE ARE OUR MAP PROPS
