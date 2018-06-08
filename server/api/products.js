26fdbca5ebb35a8a9373354092e4ce6ece18be48
Chris Perez <chrisaugustusp@gmail.com>
2018-06-08 18:06:15 -0400

    Products api routes refactored incorporating Corey's suggestions

--- server/api/products.js
+++ server/api/products.js
@@ -1,21 +1,20 @@
 const router = require('express').Router()
-const {Product} = require('../db/models')
+const { Product } = require('../db/models')
 
 //route to find single product by ID
 router.param('productId', (req, res, next, id) => {
   Product.findById(id)
-  .then(product => {
-    if (!product) {
-      const err = Error('Product not found');
-      err.status = 404;
-      throw err
-    }
-    req.product = product;
-    next();
-    return null;
-  })
-  .catch(next);
-});
+    .then(product => {
+      if (!product) {
+        const err = Error('Product not found')
+        err.status = 404
+        throw err
+      }
+      req.product = product
+      next()
+    })
+    .catch(next)
+})
 
 //All Products Route
 router.get('/', (req, res, next) => {
@@ -24,40 +23,26 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
-//Product by Name route
-router.get('/:name', (req, res, next) => {
-  const name = req.params.name;
-  Product.findByName(name)
-  .then((products) => {
-    res.json(products)
-  })
-  .catch(next)
-})
-
 //Creat New Product Route
 router.post('/', (req, res, next) => {
   Product.create(req.body)
-  .then(product => res.status(201).json(product))
-  .catch(next)
+    .then(product => res.status(201).json(product))
+    .catch(next)
 })
 
 //Update Product Route
 router.put('/:productId', (req, res, next) => {
   req.product
-  .update(req.body)
-  .then(product => res.json(product))
-  .catch(next)
+    .update(req.body)
+    .then(product => res.json(product))
+    .catch(next)
 })
 
-//Started product category route
-// router.get('/category/:category', (req, res, next) => {
-//   const category = req.params.category;
-
-//   Product.findByCategory(category)
-//   .then((products) => {
-//     res.json(products)
-//   })
-//   .catch(next)
-// })
+router.delete('/:productId', (req, res, next) => {
+  req.product
+    .destroy()
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
 
 module.exports = router
