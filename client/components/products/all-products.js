1ed895603d5b521c3a3efd6006ec9ad86161b213
Chiara Marcial Martínez <cmarmar@gmail.com>
2018-06-08 14:37:09 -0400

    star filter works!

--- client/components/products/all-products.js
+++ client/components/products/all-products.js
@@ -1,42 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 // import SingleProduct from './single-product'
 
-/**
- * COMPONENT
- */
-const Products = (props) => {
-  const products = props.state.products;
-
-  if (products.length === 0) {
-    return (
-      <div>
-        <h3>Products</h3>
-        <div>
-          <h3>There are no products to display</h3>
-        </div>
-      </div>
-    )
+//--------------Component-------------------
+class Products extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filtered: 0
+    }
+    this.handleFilter = this.handleFilter.bind(this)
   }
-  else {
+
+  handleFilter(e) {
+    let value = parseInt(e.target.name, 10)
+    console.log('here', e.target.name)
+    this.setState({ filtered: value })
+  }
+
+  render() {
+    const products = this.props.state.products || []
+    const filteredProducts = this.state.filtered
+      ? products.filter(product => {
+          return product.rating === this.state.filtered
+        })
+      : products
+    console.log(this.state)
     return (
       <div>
         <h3>Products</h3>
+        <h2>
+          <button name="1" onClick={this.handleFilter}>
+            ⭐️
+          </button>
+          <button name="2" onClick={this.handleFilter}>
+            ⭐️
+          </button>
+          <button name="3" onClick={this.handleFilter}>
+            ⭐️
+          </button>
+          <button name="4" onClick={this.handleFilter}>
+            ⭐️
+          </button>
+          <button name="5" onClick={this.handleFilter}>
+            ⭐️
+          </button>
+        </h2>
         <div className="parent-product-container">
-          {products.map((product) => {
-            let productImage = {
-              backgroundImage: `url('${product.imgUrl}')`
-            }
-            return (
-              <div key={product.id} className="product-container">
-                <div className="product-name">{product.name}</div>
-                <div className="product-imgUrl" style={productImage} />
-                <div className="product-description">{product.description}</div>
-                <div className="product-rating">{product.rating}</div>
-              </div>
-            )
-          })}
+          {filteredProducts.length ? (
+            filteredProducts.map(product => {
+              let productImage = {
+                backgroundImage: `url('${product.imgUrl}')`
+              }
+              return (
+                <div key={product.id} className="product-container">
+                  <div className="product-name">{product.name}</div>
+                  <div className="product-imgUrl" style={productImage} />
+                  <div className="product-description">
+                    {product.description}
+                  </div>
+                  <div className="product-rating">{product.rating}</div>
+                </div>
+              )
+            })
+          ) : (
+            <h2>No products available with that rating.</h2>
+          )}
         </div>
       </div>
     )
@@ -44,7 +74,7 @@ const Products = (props) => {
 }
 
 //THESE ARE OUR MAP PROPS
-const mapProducts = (state) => {
+const mapProducts = state => {
   return {
     state
   }
