c3b25441327061d789c2eae9c02b1fdb20f16db7
Lindsay Sack <lindsay.sack@gmail.com>
2018-06-12 06:48:39 -0400

    added 'isAdmin' to user model,  updated user-home component, seed file

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -6,13 +6,22 @@ import {connect} from 'react-redux'
  * COMPONENT
  */
 export const UserHome = (props) => {
-  const {email} = props
-
-  return (
+  const {email, isAdmin} = props
+  console.log(props.isAdmin)
+  return isAdmin ? (
+    <div>
     <div>
       <h3>Welcome, {email}</h3>
     </div>
+    <div>
+        <h3>MANAGE USERS</h3>
+    </div>
+    <div>
+        <h3>MANAGE PRODUCTS</h3>
+    </div>
+    </div>
   )
+  : (<h3>Welcome, {email}</h3>)
 }
 
 /**
@@ -20,7 +29,8 @@ export const UserHome = (props) => {
  */
 const mapState = (state) => {
   return {
-    email: state.user.email
+    email: state.user.email,
+    isAdmin: state.user.isAdmin
   }
 }
 
