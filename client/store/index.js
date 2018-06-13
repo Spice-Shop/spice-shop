955b88013df0a1ebea4bde0e898138ab745d051d
Daniel Castillo <dcastillo3@gmail.com>
2018-06-13 11:10:31 -0400

    updated middleware

--- client/store/index.js
+++ client/store/index.js
@@ -1,5 +1,4 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
@@ -11,8 +10,7 @@ import orderHistory from './order-history'
 
 const reducer = combineReducers({user, products, cart, location, allUsers, orderHistory})
 const middleware = composeWithDevTools(applyMiddleware(
-  thunkMiddleware,
-  createLogger({collapsed: true})
+  thunkMiddleware
 ))
 const store = createStore(reducer, middleware)
 
